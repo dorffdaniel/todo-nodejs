7ad7d063071d7a00e6e1f9ac7fb75dffe46a7428
Dorffdaniel <danielcontabill@hotmail.com>
2026-01-14 18:13:19 +0000

    feat: adicionado opcao de adicionar tarefas

--- server/app.js
+++ server/app.js
@@ -28,6 +28,50 @@ app.get('/getList', (req, res) => {
 })
 
 
+app.post('/addTask', (req, res) => {
+
+    const { titulo, descricao, estado, dia } = req.body;
+
+    let sql = "INSERT INTO tarefas(titulo, descricao, estado, data_criacao) values(?, ?, ? , ?)";
+
+    con.query(sql, [titulo, descricao, estado, dia], (error, result) => {
+        if (error) {
+            console.log(error);
+            res.status(500).json({ 'erro': 'erro ao inserir a tarefa' });
+        }
+
+        res.status(200).json({ 'mensagem': 'adicionado com sucesso' });
+    })
+
+})
+
+app.get('/getEdit/:id', (req, res) => {
+
+    const { id } = req.params;
+
+    let sql = "SELECT * FROM tarefas WHERE id = ?";
+
+    con.query(sql, id, (error, result) => {
+        if (error) {
+            console.log(error);
+            res.status(500).json({ 'erro': 'erro ao buscar a tarefa' });
+        }
+
+        if (result.length == 0) {
+            return res.status(404).json({ 'erro': 'nada encontrado' });
+        }
+
+        res.json(result[0]);
+
+    })
+
+
+});
+
+
+
+
+
 app.listen(PORT, () => {
     console.log("server runing")
     console.log("http://localhost:3001");
--- src/js/script.js
+++ src/js/script.js
@@ -14,14 +14,18 @@ async function getListTask() {
         let msg = '';
         res.forEach(element => {
             const data = new Date(element.data_criacao);
-            const dataFormatada = data.toISOString().split('T')[0];
+            const dia = String(data.getDate()).padStart(2, '0');      // dia com 2 dígitos
+            const mes = String(data.getMonth() + 1).padStart(2, '0'); // mês começa do 0
+            const ano = data.getFullYear();
+
+            const dataFormatada = `${dia}/${mes}/${ano}`;
 
             let estd = null;
             let estado = element.estado;
             if (estado == 0) {
                 estd = 'Pendente'
             } else {
-                estd = 'confirmado'
+                estd = 'Concluida'
             }
 
             msg += `<tr>`
@@ -30,10 +34,11 @@ async function getListTask() {
             msg += `<td class='text-center'> ${estd} </td>`
             msg += `<td class='text-center'> ${dataFormatada} </td>`
             msg += `<td class='text-center'>
-                <button class='btn btn-warning'> Editar </button>        
+                <button class='btn btn-warning' onclick="editTask(${element.id})"> Editar </button>        
                 <button class='btn btn-danger'>Apagar</button>        
             </td>`
             msg += `</tr>`
+
         });
 
         document.getElementById('res').innerHTML = msg;
@@ -44,6 +49,84 @@ async function getListTask() {
     }
 
 }
+
+async function addTask() {
+    let titulo = document.getElementById('titulo').value
+    let descricao = document.getElementById('descricao').value
+    let estado = document.getElementById('estado').value
+    let dia = document.getElementById('criacao').value
+
+    if (!titulo.trim() || !descricao.trim() || estado == '-1' || !dia.trim()) {
+        alert("campo vazio");
+        return;
+    }
+
+    let add = url + '/addTask';
+    let data = { titulo, descricao, estado, dia };
+
+    try {
+
+        // envio 
+        const dados = await fetch(add, {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(data)
+        });
+
+        // obtenho a respota
+        let resp = await dados.json();
+
+        console.log(`respota ${resp}`);
+        alert(resp.mensagem)
+        if (resp.mensagem) {
+            getListTask();
+        }
+
+    } catch (error) {
+        console.log("Erro: " + error)
+    }
+
+}
+
+async function editTask(id) {
+
+    let getEdit = url + '/getEdit';
+
+    let dados = await fetch(`${getEdit}/${id}`);
+
+    console.log(dados);
+
+    let res = await dados.json();
+
+    console.log(res);
+    console.log(res.id);
+    console.log(res.titulo);
+    let msg = null;
+    if (res.estado == 0) {
+        msg = 'Pendente'
+    } else {
+        msg = 'Concluida'
+    }
+
+    const data = new Date(res.data_criacao);
+    const dataFormatada = data.toISOString().split('T')[0];
+
+    if (res != null) {
+        document.getElementById('tituloEdit').value = res.titulo;
+        document.getElementById('descricaoEdit').value = res.descricao;
+        document.getElementById('estdTask').innerHTML = msg;
+        document.getElementById('estadoEdit').value = res.estado;
+        document.getElementById('criacaoEdit').value = dataFormatada;
+
+
+        let modal = document.getElementById('modalTasks');
+        let mod = new bootstrap.Modal(modal);
+        mod.show();
+    }
+
+}
+
+
 
 
 
